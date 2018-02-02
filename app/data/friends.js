3458637a7af450a9f53e646460b5840bd88d3195
Josh Silverman <jrsilverman71@hotmail.com>
2018-02-02 10:06:40 -0800

    friends array/htmlRoutes

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Big Bird',
+		photo: 'http://i.telegraph.co.uk/multimedia/archive/02869/big_bird1_2869483b.jpg'
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Grover',
+		photo: 'https://vignette.wikia.nocookie.net/muppet/images/d/d9/Grover_Groove.jpg/revision/latest?cb=20120128191914',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Cookie Monster',
+		photo: 'https://vignette.wikia.nocookie.net/iannielli-legend/images/6/6e/Cookie_monster.jpg/revision/latest?cb=20150918140937',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Snuffleupagus',
+		photo: 'https://www.thewrap.com/wp-content/uploads/2016/01/Snuffleupagus.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'The Count',
+		photo: 'https://cnet3.cbsistatic.com/img/edNENAL1oMyriVHVcjTn-BjPNrg=/fit-in/970x0/2014/12/01/9bcaf58e-5f17-46b9-82a1-546898aad693/count.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Oscar the Grouch',
+		photo: 'https://vignette.wikia.nocookie.net/muppet/images/c/c3/Oscar-can2.jpg/revision/latest?cb=20090819003837',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Ernie',
+		photo: 'https://vignette.wikia.nocookie.net/muppet/images/2/2e/ErnieFullFigure-NEW.jpg/revision/latest?cb=20100901224144',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Bert',
+		photo: 'https://vignette.wikia.nocookie.net/muppet/images/e/e1/Bert_smile.png/revision/latest?cb=20110630173259',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Kermit the Frog',
+		photo: 'http://i.dailymail.co.uk/i/pix/2015/09/02/08/2BE5406100000578-3218820-image-m-25_1441179775589.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Elmo',
+		photo: 'http://1rjm5qg4v8732kpmn44ya6bj-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/elmo-6601.png',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
