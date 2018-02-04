a8d42ff9bb1c4dbbb2755c0b27ff17de50361a40
Mark Carlson <mcarlson@avenuecode.com>
2018-02-04 15:47:31 -0800

    Fixes static content.  Ignores node_modules in git.

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.text());
+// app.use(bodyParser.text());
 
-//require(path.join(__dirname, './app/routing/apiRoutes'))(app);
-//require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+app.use(express.static('app/public'));
+
+//require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
 
 app.listen(PORT, function() {
   console.log('Friend Finder app is listening on PORT: ' + PORT);
